d88fa59c6af6fb40346a78f228ea53c800e3a272
unknown <rashidathomas1@yahoo.com>
2020-02-05 22:08:08 -0500

    Added the Counter component & state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+/*class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+<center> <strong><font color="gold"> <p> Hello World </p> </font></strong></center>
+      </div>
+    )
+  }
 }
+export default HelloWorld*/
 
-export default App;
+class Counter extends Component {
+  constructor () {
+    super();
+    this.state = {
+      count:0,
+    }
+  }
+  render() {
+  return(
+    <div></div>
+  )
+  }
+}
+export default Counter
